6526e807caef1c977e9369c2e225ed47fbd6820a
sara-yasserr <sarahyasser979@gmail.com>
2025-12-01 13:18:50 +0200

    feat: add ApplicationDbContext with EF Core configuration

---
+++ OrdersAPI.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdersAPI.Core.Models;
+using StackExchange.Redis;
+using Order = OrdersAPI.Core.Models.Order;
+
+namespace OrdersAPI.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.HasKey(e => e.OrderId);
+                entity.Property(e => e.CustomerName).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.Product).IsRequired().HasMaxLength(200);
+                entity.Property(e => e.Amount).HasPrecision(18, 2);
+            });
+        }
+    }
+}
