0743de44c4a3561cd8dc20cec7945a8b4de00718
sara-yasserr <sarahyasser979@gmail.com>
2025-12-03 04:30:55 +0200

    feat: Add redis cache service implementation

---
+++ OrdersAPI.Infrastructure/Services/RedisCacheService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using OrdersAPI.Core.Interfaces;
+using StackExchange.Redis;
+
+namespace OrdersAPI.Infrastructure.Services
+{
+    public class RedisCacheService : ICacheService
+    {
+        private readonly IConnectionMultiplexer _redis;
+        private readonly IDatabase _database;
+        public RedisCacheService(string configuration)
+        {
+            _redis = ConnectionMultiplexer.Connect(configuration);
+            _database = _redis.GetDatabase();
+        }
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var value = await _database.StringGetAsync(key);
+            if (value.IsNullOrEmpty)
+                return default;
+            return JsonSerializer.Deserialize<T>(value!);    
+        }
+
+        public async Task SetAsync<T>(string key, T value, TimeSpan ttl)
+        {
+            var serializedValue = JsonSerializer.Serialize(value);
+            await _database.StringSetAsync(key, serializedValue, ttl);
+        }
+
+        public async Task RemoveAsync(string key)
+        {
+            await _database.KeyDeleteAsync(key);
+        }
+
+    }
+}
