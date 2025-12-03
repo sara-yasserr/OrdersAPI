a9a30bc961e952e5914db177484e818b61719b1d
sara-yasserr <sarahyasser979@gmail.com>
2025-12-03 04:29:32 +0200

    chore: added configurations in program.cs
    -CORS
    -Redis
    -Swagger
    -GlobalExceptionMiddleware
    -OrderRepository

--- OrdersAPI.API/Program.cs
+++ OrdersAPI.API/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+using OrdersAPI.API.Middleware;
 using OrdersAPI.Core.Interfaces;
 using OrdersAPI.Infrastructure.Data;
+using OrdersAPI.Infrastructure.Repositories;
+using OrdersAPI.Infrastructure.Services;
 using Serilog;
 
 namespace OrdersAPI.API
@@ -12,41 +17,92 @@ namespace OrdersAPI.API
             var builder = WebApplication.CreateBuilder(args);
 
             #region Configure Serilog
-            // Configure Serilog
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Configuration)
                 .WriteTo.Console()
                 .WriteTo.File("logs/orderapi-.txt", rollingInterval: RollingInterval.Day)
                 .CreateLogger();
-            
+
             builder.Host.UseSerilog();
             #endregion
 
             // Add services to the container.
-
             builder.Services.AddControllers();
 
-            #region Configure EF with SQL Server
-            // Configure EF Core with SQL Server
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            #region Configure CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+            });
             #endregion
 
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            #region Configure Database
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    sqlOptions => sqlOptions.EnableRetryOnFailure()));
+            #endregion
+
+            #region Configure Redis
+            builder.Services.AddSingleton<ICacheService>(sp =>
+            {
+                var configuration = builder.Configuration.GetSection("Redis:Configuration").Value
+                    ?? throw new InvalidOperationException("Redis configuration is missing");
+                return new RedisCacheService(configuration);
+            });
+            #endregion
+
+            #region Register Services
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+            #endregion
+
+            #region Configure Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Orders API",
+                    Version = "v1",
+                    Description = "A RESTful API for managing orders with Redis caching",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Sara Yasser",
+                        Email = "sarahyasser979@gmail.com"
+                    }
+                });
+            });
+            #endregion
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            #region Configure Middleware Pipeline
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Orders API V1");
+                    options.RoutePrefix = string.Empty;
+                    options.DocumentTitle = "Orders API Documentation";
+                });
             }
+
+            app.UseCors("AllowAll");
+
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
             app.MapControllers();
+            #endregion
 
             try
             {
